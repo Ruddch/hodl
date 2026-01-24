e30eb16e9735a03a0af8690c193d25b4e1926de9
Semion Shishlov <semionshish@gmail.com>
2026-01-24 22:15:16 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: basePath || undefined,
-  assetPrefix: basePath ? basePath : "./",
+  assetPrefix: basePath ? basePath : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
