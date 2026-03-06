d5d5e25931ab85356e8e646816555d8a1b263a36
Semion Shishlov <semionshish@gmail.com>
2026-03-06 20:33:42 +0300

    posthog

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,7 +2,7 @@ import posthog from "posthog-js";
 
 if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_TOKEN) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_TOKEN, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://eu.i.posthog.com",
     person_profiles: "identified_only",
   });
 }
